a0966c86ecf8a1a250d7ecc8e7efe6704324287c
Muhammad Harris <harris20014@gmail.com>
2022-05-05 14:41:25 +0500

    Delete

--- Old Version/Database Application/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $connection = mysqli_connect('localhost','root','','cricket_academy');
-    if ($connection) {
-        header('location: login.php');
-    }
-    else {
-        header('location: connection_error.php');
-    }
-?>
